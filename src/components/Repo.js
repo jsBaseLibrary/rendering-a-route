c0e400ac2729516eed0dc7a17b02c1d1c7d43d52
wanghxm <wanghxm@yonyou.com>
2016-09-06 21:56:14 +0800

    增加params

---
+++ src/components/Repo.js
@@ -0,0 +1,12 @@
+// Repo.js
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>{this.props.params.repoName}</h2>
+      </div>
+    )
+  }
+})
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 import App from './components/App';
 import About from './components/About'
 import Repos from './components/Repos'
+import Repo from './components/Repo'
 import NavLink from './components/NavLink'
 
 import './styles/index.less';
@@ -10,10 +11,12 @@ import './styles/index.less';
 import {Router,Route,hashHistory} from 'react-router'
 
 render((
-  <Router history={hashHistory}>
-	<Route path="/" component={App}>
-		<NavLink path="/repos" component={Repos}/>
-	    <NavLink path="/about" component={About}/>
+   <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="/repos" component={Repos}/>
+      {/* add the new route */}
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
+      <Route path="/about" component={About}/>
     </Route>
   </Router>
 ),document.getElementById('app'))
