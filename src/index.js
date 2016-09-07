38a44ebfde94d921860b3ca0cd8296b49ac88e8d
wanghxm <wanghxm@yonyou.com>
2016-09-07 09:15:22 +0800

    取消hash地址采用browserHistory

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import NavLink from './components/NavLink'
 
 import './styles/index.less';
 
-import {Router,Route,hashHistory} from 'react-router'
+// import {Router,Route,hashHistory} from 'react-router'
+import {Router,Route,browserHistory,IndexRoute} from 'react-router'
 
 render((
-   <Router history={hashHistory}>
+   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="/repos" component={Repos}>
       	<Route path="/repos/:userName/:repoName" component={Repo}/>
